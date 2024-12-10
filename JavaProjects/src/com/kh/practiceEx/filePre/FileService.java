d4dd07ce731e704da4f65a3fcd8a195ddca7b504
coiilee <dakyung96@gmail.com>
2024-12-10 15:10:15 +0900

    File FileService-resource-catch문 활용

--- JavaProjects/src/com/kh/practiceEx/filePre/FileService.java
+++ JavaProjects/src/com/kh/practiceEx/filePre/FileService.java
@@ -1,7 +1,6 @@
 package com.kh.practiceEx.filePre;
 
-import java.io.File;
-import java.io.FileWriter;
+import java.io.*;
 
 public class FileService {
 
@@ -15,7 +14,7 @@ public class FileService {
      */
     public void createFile(String content){
         //사용자 바탕화면 경로 가져오기
-        String path = System.getProperty("user.home"+"/desktop");
+        String path = System.getProperty("user.home")+("/desktop");
         String fileName = "food.docs";
 
         File file = new File(path, fileName); //파일경로, 파일명 가져오기
@@ -28,4 +27,83 @@ public class FileService {
             System.out.println("파일 생성 / 작성 중 오류 발생 : "+e.getMessage());
         }
     }
+
+    //todo : file, filereader , bufferedreader 이용해서 파일 읽기 기능 설정
+
+    public void readFile() {
+
+        String path = System.getProperty("user.home"+"/desktop");
+        String fileName = "food.docs";
+        File file = new File(path, fileName);
+
+        try {
+            FileReader fr = new FileReader(file);
+            BufferedReader br = new BufferedReader(fr);{
+                System.out.println("["+file+"] 내용 ");
+                String line;
+                while((line= br.readLine()) !=null){ //한줄씩 반복해서 다음줄이 빈값일 때까지 출력
+                    System.out.println(line);
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("문제가 발생했습니다." + e.getMessage());
+        }
+
+
+    }
+
+    /**todo : 바탕화면에 있는 food.docs 파일에 이어쓰기 기능 설정 후 이어서 음식 작성
+     *        줄바꿈 - 햄버거 줄바꿈 돈까스 줄바꿈 쌀국수 줄바꿈 카레
+     *        File FileWriter
+     *        FileServiceRun에서 readFile 한번더 불러와 이어서 잘 작성되었는지 확인하기
+     *        제출
+     *
+     */
+    /**파일 이어쓰기 기능 설정
+     * void appendToFile(String content)
+     * @param content
+     */
+    public void appendToFile(String content) {
+        String path = System.getProperty("user.home")+"/desktop/";
+        String fileName = "food.docs";
+        File file = new File(path, fileName);
+
+        if (file.exists()) {
+            try {
+                FileWriter fw = new FileWriter(file, true);
+                fw.write(content);
+                fw.close();
+                System.out.println("이어 작성하기를 완료했습니다.");
+            } catch (IOException e) {
+                System.out.println("문제가 발생했습니다." + e.getMessage());
+            } finally {
+
+            }
+        } else {
+            System.out.println("파일이 존재하지 않습니다.");
+        }
+    }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- JavaProjects/src/com/kh/practiceEx/filePre/FileServiceRun.java
+++ JavaProjects/src/com/kh/practiceEx/filePre/FileServiceRun.java
@@ -8,9 +8,13 @@ public class FileServiceRun {
 
         //파일에 작성할 내용 :
         //맛있는 음식 목록 : 줄바꿈 해서 -피자 줄바꿈 -치킨 줄바꿈 -초밥  줄바꿈 -파스타
-        String content = "맛있는 음식 목록 : \n -피자 \n-치킨 \n -초밥 \n-파스타";
+        String content = "맛있는 음식 목록 : \n-피자 \n-치킨 \n-초밥 \n-파스타";
+        String content2 = "\n-햄버거 \n-돈까스 \n-쌀국수 \n-카레";
         //createFile 기능 호출해서 자료형 없는 매개변수명으로 파일작성내용 넣어주기
+//        fileService.createFile(content);
+//        fileService.readFile();
+        fileService.appendToFile(content2);
 
-        fileService.createFile(content);
+
     }
 }
